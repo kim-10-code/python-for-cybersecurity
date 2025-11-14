08d5265e497ec4b01a7340412b4a8ceeefe1780d
Yurii <kyy101000@gmail.com>
2025-11-13 18:16:09 -0600

    4

---
+++ automate_boring_stuff/chapter4/magic8Ball2.py
@@ -0,0 +1,14 @@
+import random
+
+messages = [
+    'It is certain',
+    'Yes definitely',
+    'Reply haze try again',
+    'Ask again later',
+    'Concentrate and ask again',
+    'My reply is no',
+    'Outlook not so good',
+    'Very doubtful'
+]
+
+print(messages[random.randint(0, len(messages) - 1)])
---
+++ automate_boring_stuff/chapter4/random_choice_shuffle.py
@@ -0,0 +1,17 @@
+import random 
+pets = ['Dog', 'Cat', 'Moose']
+print(random.choice(pets))
+print(random.choice(pets))
+print(random.choice(pets))
+print(random.choice(pets))
+
+
+people = ['Alice', 'Bob', 'Carol', 'David']
+random.shuffle(people)
+print(people)
+
+random.shuffle(people)
+print(people)
+
+random.shuffle(people)
+print(people)
